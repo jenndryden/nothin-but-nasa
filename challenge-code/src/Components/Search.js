05c82f126105e818203425f663db62abbab3c1ab
Jennifer Dryden <jenndryden8@gmail.com>
2022-01-12 22:40:30 -0500

    :hammer: fixing page bugs and adding range slider for nasa photo picker

--- challenge-code/src/Components/Search.js
+++ challenge-code/src/Components/Search.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import '@shopify/polaris/dist/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
-import {AppProvider, Page, Card, Button} from '@shopify/polaris';
+import {AppProvider, Page, Card, Button, RangeSlider} from '@shopify/polaris';
 
 //search component, allows user to search for movies and click search button 
-const Search = (props) => {
+/*const Search = (props) => {
   const [searchValue, setSearchValue] = useState("");
   
   const handleSearchInputChanges = (e) => {
@@ -35,6 +35,45 @@ const Search = (props) => {
         </AppProvider>
       </form>
     );
-}
+}*/
+const Search = (props) => {
 
+  const [searchValue, setSearchValue] = useState(32);
+
+  const handleRangeSliderChange = useCallback(
+    (value) => setSearchValue(value),
+    [],
+  );
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    //resetInputField();
+  }
+
+  return (
+    <form className="search">
+    <div class="sliderNasaCard">
+    <div class="sliderNasaCard">
+    <div class="col-md-3">
+      <RangeSlider output output step={1} max={24} min={1}
+        label="select a photo!" 
+        label="how many photos would you like to choose from?"
+        value={searchValue}
+        onChange={handleRangeSliderChange}
+        output
+      />
+      </div>
+      </div>
+
+<button class="searchbutton" onClick={callSearchFunction} type="submit" color="primary">search</button>
+    </div>
+    </form>
+  );
+
+}
 export default Search;
